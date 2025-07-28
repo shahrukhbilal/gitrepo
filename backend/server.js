0001dc37f1a679219b2924569b0ea79dd8b491b5
shahrukhbilal <southsec021karachi@gmail.com>
2025-07-28 11:01:59 -0700

    Fix: use Railway port from process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const  express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
+
 const connectDB= require('./config/dbConnection')
 const heroSliderRoutes = require('./routes/heroSliderRoutes')
 const categoryRoutes = require('./routes/categoryRoutes');
