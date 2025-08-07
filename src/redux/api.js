52bd61b614277b0ccf545b2905a2d631ae0382e1
shahrukhbilal <southsec021karachi@gmail.com>
2025-08-06 22:46:23 -0700

    api js updated

--- src/redux/api.js
+++ src/redux/api.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'https://ecom-backend-production-e2cb.up.railway.app/api',
+
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) headers.set('Authorization', token);
