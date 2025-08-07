9726dc7c3fdb55a2858fc8d639ef78ec54798267
shahrukhbilal <southsec021karachi@gmail.com>
2025-08-07 10:37:17 -0700

    contactpage updated

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -40,7 +40,7 @@ const ContactPage = () => {
 
     try {
       setLoading(true);
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
